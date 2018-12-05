aba4c5701eee36a33df4e55f4d055524725ef063
Alan Hong <xiupan@gmail.com>
2018-12-05 13:53:04 -0600

    Day 2 Part 2 Complete

---
+++ day-02/index-b.js
@@ -0,0 +1,298 @@
+const _ = require('lodash')
+
+const rawInput = String.raw`efmyhuxcqqldtwjzvisepargvo
+efuyhuxckqldtwjrvrsbpargno
+efmyhuxckqlxtwjxvisbpargoo
+efmyhuxczqbdtwjzvisbpargjo
+efmyhugckqldtwjzvisfpargnq
+tfmyhuxckqljtwjzvisbpargko
+efmyhuxckqldtvuzvisbpavgno
+efmyhufcrqldtwjzvishpargno
+tfmyhuxbkqlduwjzvisbpargno
+efayhtxckqldbwjzvisbpargno
+efgyhuxckuldtwjzvisbpardno
+efmyhuxckuldtwizvisbpargqo
+efmyhuxcknldtjjzvihbpargno
+efmyhuxcnqddtwjzvisbpafgno
+efmyhubokqldtwjzvisbpargdo
+efmhhuxckqldtwdzvisbpjrgno
+efmyhuxckqldtwjrcisbpargny
+efmyhuxckqsdtwjzlisbpargng
+effyhuxckqlqtwjzjisbpargno
+nfmyhjxckqldtwjzcisbpargno
+efmyhvxckqldtwszvwsbpargno
+efmyhuxckqldtwutvisbpprgno
+kfmyhuxckqldtwzuvisbpargno
+efhyhtxckqldtwjmvisbpargno
+efmyhuhckqldtwjzvisbpxwgno
+efmyhuxcfqldtrjzvitbpargno
+efmyhudckqldtwjfvisbparvno
+ekmyhuxckqlstwjzvisbdargno
+efmyhuxckqlxtwjftisbpargno
+etsyhuxckqldtwjzvisbpargnf
+exmyhusckqldtwjzvisbpakgno
+efmyhubckqlrtljzvisbpargno
+efmyhuxckwldtwjovizbpargno
+efmyhulckqzdtwjzvisbpargpo
+efmyhuxckbcdlwjzvisbpargno
+zfmyhulckqbdtwjzvisbpargno
+efmyquxckfldtwazvisbpargno
+efxyhuxakqldtwjzvisupargno
+efmlhuxckkedtwjzvisbpargno
+efhyhuxwkqldtwjzvisbparjno
+efmyhuxfkqldtwjzvisvparyno
+efmyhuxckqfdtijzvisblargno
+efmyhuxckqldtfjzvisbwhrgno
+efmymuxcknldtwzzvisbpargno
+eomybuxckqldtwkzvisbpargno
+pfmyhuxckqldtwgzvasbpargno
+vfmyhuxcoqldtwjzvisbparvno
+eflyhuxckqldtwjzvirypargno
+efmyvuxckqldtwizvisbpaqgno
+epmyhuxckqldtwjzvesbparpno
+efoyhuxckoldtwjmvisbpargno
+efmyhuxckqydtwpzvisbpaqgno
+efmyhuxckqldezbzvisbpargno
+efmyhuxckqldtwjzvisboalxno
+efmyhuxckqldtwuzvipbjargno
+efmymuxcuqldtwjzvasbpargno
+jfmyhuxckqldtwjzvzsbpargdo
+nfmyhuxckqlntsjzvisbpargno
+efmxhuxckqgdtwjzvisbparjno
+efmyhuxckpldtpjzvpsbpargno
+efmyhuxcyqldtwjhvisbpargqo
+efmyhexgkqydtwjzvisbpargno
+ffmyhuxckqldtwjzvisbpafgnk
+efmyfuxckqldtwjpvisbpartno
+efmyhoxckcmdtwjzvisbpargno
+efmyhuxxkqldtwjzviabparyno
+jfmyhuxakqldtwgzvisbpargno
+efmjhuxckqcdtwjzvisbjargno
+efmyhuxccqldtwjzxisbxargno
+efmyhurckqldiwjzvrsbpargno
+efmyhuxckasdtwjzvisboargno
+efmyhvxckmldtwjgvisbpargno
+efmyhuxckoldtwjuvisbpardno
+efmyduxckqldtwjgvzsbpargno
+ejmyhuxckqldtwbzvisbpargnb
+efmymuxchqldtwjzvibbpargno
+efmyhjxckqldtwjgvinbpargno
+efmyhuxhyqldtwbzvisbpargno
+efmyhuxckqldtwjzvisbpzignq
+efmyuueckqldxwjzvisbpargno
+qfmyhyxckqldtwizvisbpargno
+efmyhupckqldtwjzvpgbpargno
+efmycuxckqldtwjzvfdbpargno
+efmyhugcrqldtwjfvisbpargno
+efmyhexckqldtwjzvischargno
+efmyhuxckqldtljzvasbpamgno
+efmyzdxckqldtwjovisbpargno
+efmyhuxccqldtwjzvdsbpaigno
+ufmyhuxekqldtwjzvisbpargne
+efmyhuxckqldfwozvisgpargno
+afmyhuackqldtwjzvisbdargno
+efmyauxckqldtwjzvisiparmno
+efmysuxckqldtwjzvisbeaggno
+efmyhuxckqldtwjzvisbgzigno
+efryhuxlkqldtwozvisbpargno
+lfmyhuxckqldtwjzvhsbparuno
+efmyhzxckqldswjzvisqpargno
+efmyhuxrkqldtwjzvisgpargco
+efmyhudckqldtwjzyisbkargno
+efmyhuacqqldtwjzviabpargno
+jfmyhuxckqldtwvzvicbpargno
+efmkhuxckqlftejzvisbpargno
+nfmyhuxckqldnwjzvisbxargno
+efmyhuxckqldtwjvvisjpyrgno
+efmyhuxcmxldtwjzvisbpargto
+efmyhuxckqldtwqbvpsbpargno
+efmyhuxckzldjwjzvisbplrgno
+efmywgxckqldtwxzvisbpargno
+efmsguxckqldhwjzvisbpargno
+nfmyhuxlkqldtwjzvisbgargno
+etmyhuxckqldtwjzvqsbptrgno
+efmyxuxckqldtfjzvisbyargno
+cfmihuxckqldtwjzvisbpargnf
+jfzyhuxckqldtwjzviscpargno
+efmyhuxckqldtmjzvisbpbzgno
+bfmyhuzckqldcwjzvisbpargno
+efmyhuxckqldtmjzvmslpargno
+efqyvuxckqldtwazvisbpargno
+efmecrxckqldtwjzvisbpargno
+efmyhuuckqldtwjzvisrpargnt
+efmphuxckqldtwjzvisbparmho
+ifmyhuxckqldtwjzvismpsrgno
+efmyhuookqldywjzvisbpargno
+efmyhfxckyldtwjnvisbpargno
+efmyhxhckqldtwjzvisqpargno
+efryhuxcfqldtwjzvisbparkno
+efmyhutckqldpwjzvixbpargno
+efmyoukckqldtwjzvisbpargko
+efmyhuxckqldtwjzviseparynv
+efmyhuxcksldvwjzvisbnargno
+efmyhuxckqrdtwlzmisbpargno
+efmyhuxcwqldtwjzviqapargno
+eymyhuxckqrdtwkzvisbpargno
+efmyhuxckqldtwjzpisopargnj
+efmyhuxikqldtwjzvirupargno
+efmyhuxcuzldtnjzvisbpargno
+efmyhxxikqldtwjzvisbpalgno
+efmyhuxceqldtwjzvdsbparguo
+efmyhuxwkqldtwjmvisbparxno
+efmyhuxpkqldtwjzvisfpargfo
+efmyfuxckaldtwjzvirbpargno
+efmyhuxckqrdtwjzvismprrgno
+efmyhuxckqldzwjzvisbpnrgfo
+efmyhfuckqldtwjyvisipargno
+efmyhuxcpqlqfwjzvisbpargno
+efmyyuxckqldtwjzvrsepargno
+efmphuxckqlptqjzvisbpargno
+efmyhuxnfqldtwjzvisbpmrgno
+efmyhuxckqldtwjzkisnpnrgno
+mfmyhuxckqldtwjzvisbzarcno
+efmyhuxckqldtwlzviszpargwo
+efmytuxckqndtwjqvisbpargno
+efmyzuxckqldtwjzvisbaargjo
+efmihuxckqlutwjzvimbpargno
+efmyhuxckqldgwjzvixbparono
+tfmyduxckqldtyjzvisbpargno
+ejmyhuockqldtwjzvidbpargno
+efmyheyckqkdtwjzvisbpargno
+efmyhuxckqldtwjzoisbpargfj
+efqyhuxcxqldtwxzvisbpargno
+jfmyhaxckqldtwjzvisbvargno
+hfmyhqxckqldtwjzvisbparvno
+efmyhukckqlrtwjzvqsbpargno
+efmyhuxckqldvwmzvisbparrno
+efoyhuxckqldtwjzvilwpargno
+ejmyhuxckqldtwjzxisbprrgno
+efmyhuxckqldtsjzvisupdrgno
+efzyhjxckqldtwjzvisbpasgno
+ebmyhulckqldtwjzvisbpargnr
+efmyhuxcjqlntwjzqisbpargno
+efmlocxckqldtwjzvisbpargno
+efmyhuxckqldtwjzvizkpargnm
+ebmyhuxckqldtwjzvlfbpargno
+efmyhuxckqldtwjyvisbpjrgnq
+afmyhuxckqldtwjzvpsbpargnv
+efmyxuxckqwdzwjzvisbpargno
+efmyhuxskqlqthjzvisbpargno
+efmyhuxckqldtwdzvisbearglo
+mfmyhuxckqldtzjzvisbparggo
+efmyhuqckqodtwjzvisbpadgno
+efmyhuxctqldywjzvisspargno
+efmyhuxckqqdtwjnvisbporgno
+efmyhixckqldowjzvisbpaagno
+efmyhuxckqldtsszvisbpargns
+edmyhuxckqpdtwjzrisbpargno
+efsyhuxckqldtijzvisbparano
+efmyhuxckqxdzwjzviqbpargno
+efmyhuxckqldtwjzviqqpsrgno
+efmyhuockqlatwjzvisbpargho
+efmyhuxckqldtwjzvishkavgno
+vfmyhuxckqldtwjzvksbaargno
+efmahuxckqudtwbzvisbpargno
+ewmyhixckqudtwjzvisbpargno
+efmywuxczqldtwjzvisbpargao
+efmyhuqjkqldtwyzvisbpargno
+efmyhuxekqldtwjzmksbpargno
+efmyhuxcoqtdtwjzvinbpargno
+ebmyhuxkkqldtwjzvisbdargno
+ecmyhnxckqldtwnzvisbpargno
+efmyhuxbkqldtwjzvksbpaigno
+efayhuxckqidtwjzvisbpavgno
+efmrhuxckqldswjzvisbpaugno
+efmyhuuckqldtwjyvisipargno
+xfmyhuxckqldawjzvosbpargno
+efmyhuxckklhtwjzvisbpargnq
+efmyhmxcaqldzwjzvisbpargno
+efiyhuxcksldtwjzvisbpamgno
+zfmyhuzckqldtwjzvisbparhno
+efmyhuxckqlvtwjdvisbparsno
+efmyhmxckaldtwjzmisbpargno
+efmysuxcqoldtwjzvisbpargno
+efmyhuxckqldtwjzvisbsargrb
+effyhuxckqldtwjzvisbpwfgno
+efmyhuxclqmdtwjzxisbpargno
+edmohuxckqldtwjziisbpargno
+efmyhuxckpldtwjzviubpaegno
+efmyhuxcpqldtwjzjimbpargno
+ehmyhuxckqldtwjzsisbpargnq
+efmyhcxcdqldtwjzvisbqargno
+efmjhuxckqldmwjzviybpargno
+efeyhzxckqlxtwjzvisbpargno
+efmyhuxczqadtwazvisbpargno
+efmahuxckqldtwjzvisbpafgnl
+efmyouxckqldtwjzvizbpacgno
+emmrhuxckqldtwjzvisqpargno
+exmyhuxckqlftwjnvisbpargno
+efuyhuxckqldrwjzvisbpargnw
+efmywuxfkqldtwjztisbpargno
+efmyhuxdkqldtwjzvisbpqrzno
+eemyhuxckqldrwjzvisbpajgno
+efmyiuxckqldtbjzvrsbpargno
+eqmyhuxckqldlwjzfisbpargno
+efmyhuxckqlitwuzvisbpvrgno
+ecoyhuxckqldtwjzvishpargno
+efmyhuxckcldtwjzlisbparlno
+efmyhsxcknldtwjfvisbpargno
+efmyhuxckqldtwjrvosbpargbo
+enmehuxckzldtwjzvisbpargno
+hfmyhuxckqqdtwjzvisbpawgno
+efmyhufckcjdtwjzvisbpargno
+efmxhuxckqldthjzvisfpargno
+efmyaukckqldtwjsvisbpargno
+efmyhukckqldtwpzvisbpmrgno
+dfmyhuxckqldtwjzvisbvarmno
+afmbhuxckqldtwjzvssbpargno
+efmyhuxchqldtwezvisbpargzo
+efmphuxckqlxjwjzvisbpargno
+efhyxuxckqldtwjzvisbpargko
+sfmyhexckqldtwjzvisbqargno
+efmghuxckqldtwjzvitbparnno`
+
+const testInput = ['abcde', 'fghij', 'klmno', 'pqrst', 'fguij', 'axcye', 'wvxyz']
+
+const inputArray = rawInput.split("\n")
+
+//
+const daFunction = (input) => {
+  let outputString = ''
+  for (let aa = 0; aa < input.length; aa++) {
+    for (let ab = 0; ab < input.length; ab++) {
+      let str1 = input[aa]
+      let str2 = input[ab]
+      let aStr1 = str1.split('');
+      let aStr2 = str2.split('');
+
+      let totalCharacters;
+      if(aStr1.length > aStr2.length) {
+        totalCharacters = aStr2.length
+      } else {
+        totalCharacters = aStr1.length
+      }
+
+      let diff = [];
+      // for(let a = 0; a < totalCharacters; a++) {
+        //   if(aStr1[a] != aStr2[a]) {
+          //     diff.push(aStr1[a]); // or something else
+          //   }
+          // }
+          for(let a = 0; a < totalCharacters; a++) {
+            if(aStr1[a] === aStr2[a]) {
+              diff.push(aStr1[a]); // or something else
+            }
+          }
+          // if (diff.length < input[0].length && diff.length != 0) {
+          //   console.log(diff);
+          // }
+          if (diff.length > (input[0].length - 2) && diff.length != 0 && diff.length != input[0].length) {
+            // console.log(diff);
+            outputString = diff.join("")
+            console.log(outputString);
+          }
+        }
+      }
+}
+
+daFunction(inputArray)
